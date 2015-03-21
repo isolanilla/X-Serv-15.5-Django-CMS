b67bdc4d067553aec6bb288f39ee074e289ad24a
isolanilla <i.solanilla3@gmail.com>
2015-03-22 00:11:08 +0100

    ejercicio con el put

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from models import Pages
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def cms(request, recurso):
+
+    if request.method == "GET":
+        try:
+            clave = Pages.objects.get(name=recurso)
+            return HttpResponse("nombre: " + clave.name + " " + clave.page)
+
+        except Pages.DoesNotExist:
+            return HttpResponseNotFound("No encontrado recurso: " + recurso)
+
+    if request.method == "PUT":
+        p = Pages(name=recurso, page=request.body)
+        p.save()
+        HttpResponseNotFound
+        return HttpResponse("Pagina guardada: " + request.body)
